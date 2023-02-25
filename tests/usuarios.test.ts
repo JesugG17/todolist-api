867f54202b9741885f3fe72748e214b216123955
Jesus Gastelum <gastelumtec21@gmail.com>
2023-02-24 17:46:03 -0700

    renamed test file and added a new test with GET method

--- tests/usuarios.test.ts
+++ tests/usuarios.test.ts
@@ -4,13 +4,14 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 
 import { Usuarios } from '../routes';
+import { StatusCodes } from 'http-status-codes';
 
 chai.use( chaiHttp );
 
 const expect = chai.expect;
 const url = 'http://localhost:8080/'
 
-describe('POST /usuarios', () => {
+describe('POST /usuarios ERR', () => {
     it('Retorna un BAD REQUEST porque el correo ya esta registrado', (done: Done) => {
         chai.request(url)
         .post('v1/api/usuarios')
@@ -19,7 +20,7 @@ describe('POST /usuarios', () => {
             password: '123456'
         })
         .end((err, res) => {
-            expect(res).to.have.status(400);
+            expect(res).to.have.status(StatusCodes.BAD_REQUEST);
             done();
         })
     });
@@ -31,12 +32,26 @@ describe('POST /usuarios', () => {
         chai.request(url)
         .post('v1/api/usuarios')
         .send({
-            correo: 'miamor@gmail.com',
+            correo: 'miamor1@gmail.com',
             password: '123456'
         })
         .end((err, res) => {
-            expect(res).to.have.status(201);
+            expect(res).to.have.status(StatusCodes.CREATED);
             done();
-        })
+        });
     });
+});
+
+describe('GET /usuarios', () => {
+    it('Retorna un status 200 obteniendo a todos los usuarios', (done: Done) => {
+        chai.request(url)
+        .get('v1/api/usuarios')
+        .end((err, res) => {
+
+            expect(res).to.have.status(StatusCodes.OK);
+            expect(res.body).not.to.be.empty;
+
+            done();
+        });
+    })
 });
