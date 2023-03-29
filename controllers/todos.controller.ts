76d35692716b3a1edf7a7db032468a3ea2d82d68
Jesus Gastelum <gastelumtec21@gmail.com>
2023-03-29 07:48:57 -0700

    TodosService done

--- controllers/todos.controller.ts
+++ controllers/todos.controller.ts
@@ -1,59 +1,28 @@
 import { Request, Response } from "express";
-import { uuid } from 'uuidv4';
-import { Todo } from '../models/Todo.model';
+import { TodosService } from "../services/todos.service";
 
 export const getTodos = async(req: Request, res: Response) => {
 
-    const { offset = 0, limit = 100 } = req.query;
-    const usuarioid = req.usuario?.usuarioid;
-
-    const { count, rows: todos } = await Todo.findAndCountAll({
-        where: {
-            estatus: true,
-            usuarioid
-        },
-        offset: Number(offset),
-        limit: Number(limit)
-    });
+    const { count, todos } = await TodosService.findAll(req);
 
     res.json({ count, todos });
 }
 
 export const createTodo = async(req: Request, res: Response) => {
 
-    const { description } = req.body;
-    
-    const data = {
-        todoId: uuid(),
-        description,
-        estatus: true,
-        usuarioid: req.usuario?.usuarioid
-    };
-
-    console.log( data );
-    const todo = await Todo.create(data);
+    const todo = await TodosService.create(req);
 
     res.json({ todo });
 }
 
 export const updateTodo = async(req: Request, res: Response) => {
 
-    const { id } = req.params;
-    const { description } = req.body;
+    const todo = await TodosService.update(req);
 
-    const todo = await Todo.findByPk( id );
-
-    const newTodo = await todo?.update({ description });
-
-    res.json({ todo: newTodo });
+    res.json({ todo });
     
 }
 
 export const deleteTodo = async(req: Request, res: Response) => {
-    const { id } = req.params;
-
-    const todo = await Todo.findByPk( id );
-    
-    await todo?.destroy();
-    res.json({ todo });
+    await TodosService.delete(req);
 }
--- services/todos.service.ts
+++ services/todos.service.ts
@@ -1,2 +1,52 @@
 // TODO: HACER EL SERVICE DE LOS TODOS
 
+import { Request } from "express";
+import { v4 as uuid } from 'uuid';
+import { Todo } from '../models/Todo.model';
+
+
+export class TodosService {
+
+    static async findAll(req: Request) {
+        const usuarioid = req.usuario?.usuarioid;
+    
+        const { count, rows: todos } = await Todo.findAndCountAll({
+            where: {
+                estatus: true,
+                usuarioid
+            },
+        });
+
+        return { count, todos };
+    }
+
+    static async create(req: Request) {
+        const { description } = req.body;
+    
+        const data = {
+            todoId: uuid(),
+            description,
+            estatus: true,
+            usuarioid: req.usuario?.usuarioid
+        };
+
+        return await Todo.create(data);
+    }
+
+    static async update(req: Request) {
+        const { id } = req.params;
+        const { description } = req.body;
+
+        const todo = await Todo.findByPk( id );
+
+        return await todo?.update({ description });
+    }
+
+    static async delete(req: Request) {
+        const { id } = req.params;
+
+        const todo = await Todo.findByPk( id );
+        
+        await todo?.destroy();
+    }
+}
