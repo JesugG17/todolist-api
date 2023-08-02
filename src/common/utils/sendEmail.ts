7c2a9a0c7300c0492822c9c1f3a7bdce7d190d80
Jesus Gastelum <gastelumtec21@gmail.com>
2023-08-01 20:33:39 -0700

    change service to host

--- src/common/utils/sendEmail.ts
+++ src/common/utils/sendEmail.ts
@@ -3,7 +3,7 @@ import config from "../../../config";
 import { Users } from "../../modules/auth/entities";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: 'smtp.gmail.com',
   port: 465,
   secure: true,
   auth: {
