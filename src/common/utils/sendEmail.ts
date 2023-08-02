1c856d7f61727db4b6e162c07d714bed3bf1322a
Jesus Gastelum <gastelumtec21@gmail.com>
2023-08-01 20:41:37 -0700

    trying to fix problem on production

--- src/common/utils/sendEmail.ts
+++ src/common/utils/sendEmail.ts
@@ -5,7 +5,7 @@ import { Users } from "../../modules/auth/entities";
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: true,
+  secure: false,
   auth: {
     user: config.EMAIL,
     pass: config.PASSWORD,
